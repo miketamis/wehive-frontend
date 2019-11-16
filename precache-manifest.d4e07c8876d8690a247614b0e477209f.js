5ac5d05032c55d14df903fea730c2f50630f3fae
Mike Tamis <mike.tamis@spark.co.nz>
2019-11-17 08:08:30 +1300

    Updates

---
+++ precache-manifest.d4e07c8876d8690a247614b0e477209f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82f6a609c35a22cfdcd00468e712f20",
+    "url": "/wehive-frontend/index.html"
+  },
+  {
+    "revision": "cff8178a7134d1990088",
+    "url": "/wehive-frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "628a41ac17c2b4c41aef",
+    "url": "/wehive-frontend/static/js/2.3407c18c.chunk.js"
+  },
+  {
+    "revision": "cff8178a7134d1990088",
+    "url": "/wehive-frontend/static/js/main.90e90917.chunk.js"
+  },
+  {
+    "revision": "41663ca2c91638ddb3d0",
+    "url": "/wehive-frontend/static/js/runtime-main.6f98731e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wehive-frontend/precache-manifest.d4e07c8876d8690a247614b0e477209f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wehive-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
